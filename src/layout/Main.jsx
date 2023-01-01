1886329d6f6b7b3c04c2d21d2585affe4d4a9849
imara abdullah <102338647+imaraabdullah0@users.noreply.github.com>
2023-01-01 21:08:16 +0200

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Main = () => {
+  return (
+
+<>
+    <Navbar />
+
+    <div>Main</div>
+</>
+  )
+}
+
+export default Main
